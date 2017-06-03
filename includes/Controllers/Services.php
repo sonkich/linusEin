a07ab0ac6830a908da07fc452276b0c0458a4810
Antonio Dyulgerski <adyulgerski94@gmail.com>
2017-06-03 15:26:40 +0300

    home views added

---
+++ includes/Controllers/Services.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Services extends Controller {
+
+
+    public static function constructView($name, $data = []) {
+
+        $data['title'] =   $name;
+
+        View::createView("services_$name" ,$data);
+    }
+}
--- includes/routes/Routes.php
+++ includes/routes/Routes.php
@@ -8,6 +8,34 @@ Route::set('home', function() {
     Home::constructView();
 });
 
+Route::set('services/office', function() {
+    Services::constructView('office');
+});
+
+Route::set('services/home', function() {
+    Services::constructView('home');
+});
+
+Route::set('services/wash', function() {
+    Services::constructView('wash');
+});
+
+Route::set('services/renovation-clean', function() {
+    Services::constructView('renovation-clean');
+});
+
+Route::set('prices', function() {
+    Home::constructView();
+});
+
+Route::set('contacts', function() {
+    Home::constructView();
+});
+
+Route::set('partners', function() {
+    Home::constructView();
+});
+
 Route::set('404', function() {
     View::createView('404',[]);
 });
